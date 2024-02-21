7b01407cf013b9aa46d84e65ec640f72869dce3f
purplebearr <82906187+purplebearr@users.noreply.github.com>
2024-02-21 00:21:48 -0800

    Added chunk unloading mechanism

--- LandscapeGenerator.cpp
+++ LandscapeGenerator.cpp
@@ -15,6 +15,7 @@ ALandscapeGenerator::ALandscapeGenerator()
 void ALandscapeGenerator::BeginPlay()
 {
 	Super::BeginPlay();
+    GetWorldTimerManager().SetTimer(TimerHandle, this, &ALandscapeGenerator::TimerTick, TimerInterval, true);
 }
 
 void ALandscapeGenerator::Tick(float DeltaTime)
@@ -36,10 +37,6 @@ void ALandscapeGenerator::Tick(float DeltaTime)
         Current.X = FMath::TruncToInt(MyCharacter.X/Scale);
         Current.Y = FMath::TruncToInt(MyCharacter.Y/Scale);
 
-        //test calculations
-        float calc1 = FMath::TruncToInt(MyCharacter.X / Scale);
-        float calc2 = FMath::TruncToInt(MyCharacter.Y / Scale);
-
         for(int i = (Distance*-1); i <= Distance; i++)
         {
             X = i;
@@ -51,8 +48,9 @@ void ALandscapeGenerator::Tick(float DeltaTime)
                 Visible.X = Current.X + i;
                 Visible.Y = Current.Y + j;
 
-                if (GeneratedChunkLocations.Contains(Visible))
+                if (GeneratedChunks.Contains(Visible))
                 {
+
                     //GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow, FString::Printf(TEXT("contains visible")));
                 }
                 else 
@@ -63,27 +61,46 @@ void ALandscapeGenerator::Tick(float DeltaTime)
 
                     ADiamondSquare* NewDiamondSquare = GetWorld()->SpawnActor<ADiamondSquare>(ADiamondSquare::StaticClass(), SpawnLocation, SpawnRotation, SpawnInfo);
 
-
                     NewDiamondSquare->Size = this->Size;
                     NewDiamondSquare->Zmultiplier = this->Zmultiplier;
                     NewDiamondSquare->UVScale = this->UVScale;
                     NewDiamondSquare->Material = this->Material;
                     NewDiamondSquare->VertexDistance = (this->Scale)/(this->Size);
                     NewDiamondSquare->NoiseParameters = this->NoiseParameters;
-
                     NewDiamondSquare->Xoffset = Size * Visible.X;
                     NewDiamondSquare->Yoffset = Size * Visible.Y;
-
-
-
                     NewDiamondSquare->GenerateChunk();
 
-
-                    GeneratedChunkLocations.Add(Visible);
-                    
+                    //GeneratedChunkLocations.Add(Visible);
+                    GeneratedChunks.Add(Visible, NewDiamondSquare);
 
                 }
             }
+        }
+    }    
+    
+}
+
+void ALandscapeGenerator::TimerTick()
+{
+    // This function will be called every 5 seconds
+    // GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow, FString::Printf(TEXT("tick tic")));
+    for (auto& Elem : GeneratedChunks)
+    {
+        FChunkPosition ChunkPosition = Elem.Key;
+        ADiamondSquare* Chunk = Elem.Value;
+
+        // Calculate the world position of the chunk
+        FVector ChunkWorldPosition(ChunkPosition.X * Scale, ChunkPosition.Y * Scale, 0.0f);
+
+        // Calculate the distance from the player to the chunk
+        float DistanceToPlayer = FVector::Dist(MyCharacter, ChunkWorldPosition);
+
+        // If the chunk is too far from the player, destroy it
+        if (DistanceToPlayer > MaxChunkDistance)
+        {
+            Chunk->Destroy();
+            GeneratedChunks.Remove(ChunkPosition);  // Remove the chunk from the map
         }
     }
 }
